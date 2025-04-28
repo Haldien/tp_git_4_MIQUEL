8da51af8af3ba58051b3f599b286fff537280be1
Haldien <minhkim.miquel@gmail.com>
2025-04-28 10:29:36 +0200

    Les tests sont maintenant ordonnés, ajout des tests de toMinutes()

--- src/test/java/test/HoraireTest.java
+++ src/test/java/test/HoraireTest.java
@@ -2,8 +2,12 @@ package test;
 
 import modele.Horaire;
 
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class HoraireTest {
 
     @org.junit.jupiter.api.BeforeEach
@@ -19,11 +23,18 @@ public class HoraireTest {
     Horaire h1 = new Horaire(9,15);
     Horaire h2 = new Horaire(10,45);
     Horaire h3 = new Horaire(9,15);
+    Horaire h4 = new Horaire(0,30);
 
-    @org.junit.jupiter.api.Test
+    @org.junit.jupiter.api.Test @Order(2)
     void compareTo() {
         assertEquals(-1,h1.compareTo(h2));
         assertEquals(1,h2.compareTo(h1));
         assertEquals(0,h1.compareTo(h3));
+    }
+
+    @org.junit.jupiter.api.Test @Order(1)
+    void toMinutes() {
+        assertEquals(555,h1.toMinutes());
+        assertEquals(30,h4.toMinutes());
     }
 }

423478e8e00e3e7b53781cc887422c7a34fca51e
Haldien <minhkim.miquel@gmail.com>
2025-04-28 10:41:25 +0200

    Ajout de PlageHoraire et sa classe de test

---
+++ src/test/java/test/PlageHoraireTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import modele.PlageHoraire;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlageHoraireTest {
+}

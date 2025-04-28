287fb2bbb0e6640732251fc59d1c421690fe9ba5
Haldien <minhkim.miquel@gmail.com>
2025-04-28 10:55:59 +0200

    Tests duree()

--- src/main/java/modele/PlageHoraire.java
+++ src/main/java/modele/PlageHoraire.java
@@ -1,4 +1,55 @@
 package modele;
 
-public class PlageHoraire {
+public class PlageHoraire implements Comparable<PlageHoraire> {
+
+    // final : constant; static : associé à la classe et non à un objet particulier
+    private final static int DUREE_MIN = 30; // durée min : 30 minutes, arbitraire
+    private Horaire chHoraireDebut;
+    private Horaire chHoraireFin;
+
+    public PlageHoraire(Horaire parHoraireDebut, Horaire parHoraireFin) {
+
+        chHoraireDebut = parHoraireDebut;
+        chHoraireFin = parHoraireFin;
+
+    }
+
+    public String toString(){
+        return "Debut : " + chHoraireDebut + ", Fin : " +chHoraireFin + ", Duree : " + this.duree();
+    }
+
+    public boolean estValide(){
+        if (chHoraireDebut.estValide() && chHoraireFin.estValide()){
+            if(chHoraireFin.toMinutes() >= chHoraireDebut.toMinutes() + DUREE_MIN){ // Non seulement l'horaire de fin doit être plus grande que l'horaire de début, il faut aussi prendre en compte la durée minimum
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Horaire duree(){ // On considère que c'est une Horaire. On pourrait peut-être le faire avec un int
+
+        Horaire duree = new Horaire(0, 0);
+
+        int enMinutes = chHoraireFin.toMinutes() - chHoraireDebut.toMinutes();
+
+        duree.setHeure(enMinutes/60);
+        duree.setQuartHeure(enMinutes%60);
+
+        return duree;
+
+    }
+
+    public int compareTo(PlageHoraire p2){
+        if (this.chHoraireFin.compareTo(p2.chHoraireDebut) <= 0){ // comparaison non-stricte : pour les cas comme 9H0-10h30 et 10h30-11h30. Ces plages ont 10h30 en commun mais la premmière précède la deuxième
+            return -1;
+        }
+        else if (this.chHoraireDebut.compareTo(p2.chHoraireFin) >= 0){
+            return 1;
+        }
+        return 0;
+    }
+
+
+
 }
--- src/test/java/test/PlageHoraireTest.java
+++ src/test/java/test/PlageHoraireTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import modele.Horaire;
 import modele.PlageHoraire;
 
 import org.junit.jupiter.api.TestMethodOrder;
@@ -7,5 +8,37 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class PlageHoraireTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        System.out.println("setup");
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+        System.out.println("teardown");
+    }
+
+    Horaire startHoraire1 = new Horaire(8, 15);
+    Horaire endHoraire1 = new Horaire(13, 30);
+    Horaire startHoraire2 = new Horaire(10, 45);
+    Horaire endHoraire2 = new Horaire(11, 45);
+    Horaire startHoraire3 = new Horaire(13, 0);
+    Horaire endHoraire3 = new Horaire(21, 0);
+    Horaire startHoraire4 = new Horaire(9, 30);
+    Horaire endHoraire4 = new Horaire(10, 30);
+
+    PlageHoraire plageHoraire1 = new PlageHoraire(startHoraire1, endHoraire1);
+    PlageHoraire plageHoraire2 = new PlageHoraire(startHoraire2, endHoraire2);
+    PlageHoraire plageHoraire3 = new PlageHoraire(startHoraire3, endHoraire3); // PAS VALIDE
+    PlageHoraire plageHoraire4 = new PlageHoraire(startHoraire4, endHoraire4);
+
+
+    @org.junit.jupiter.api.Test
+    void duree(){
+        assertTrue(plageHoraire1.duree().compareTo(new Horaire(5, 15)) == 0);
+        assertTrue(plageHoraire3.duree().compareTo(new Horaire(8, 0)) == 0);
+    }
+
 }
